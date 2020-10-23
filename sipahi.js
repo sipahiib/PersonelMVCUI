f5c4b56ee59158f86760a1b230be04c8f8756acf
İbrahim <73345010+sipahiib@users.noreply.github.com>
2020-10-23 16:02:05 +0300

    git satırı

--- sipahi.js
+++ sipahi.js
@@ -1,4 +1,4 @@
-﻿//ekrandaki kaydı silerken, servera gitmeden ajax ile silmeyi sağlar.  
+﻿//ekrandaki kaydı silerken, servera gitmeden ajax ile silmeyi sağlar. git!
 $(function () {
     $("#tblDepartmanlar").DataTable({
         "language": {
